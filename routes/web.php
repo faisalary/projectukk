e2fff9dd842eec60daf50c0d7236d1e183112f3d
Fauzi Aulia <fauziaulia@student.telkomuniversity.ac.id>
2023-10-31 11:51:14 +0700

    fix login

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\Front\JobController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Middleware\isApplicant;
+use App\Http\Controllers\Auth\LoginAdminController;
+use App\Http\Controllers\Auth\RegisterAdminController;
+use App\Http\Controllers\Auth\SetPasswordController;
+use App\Http\Middleware\IsAdmin;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,7 +23,7 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'isAdmin','verified'])->name('dashboard');
 
 
 
@@ -51,7 +52,55 @@ Route::group(['middleware' => isApplicant::class], function () {
 
 
 require __DIR__.'/auth.php';
+Route::get('/pengaturan', function () {
+    return view('pengaturan_akun.pengaturan_akun');
+});
 
 
+Route::get('/apply_alert', function () {
+    return view('apply.apply_alert');
+});
+
+
+//untuk tampilan home
 Route::get('/search', [App\Http\Controllers\Front\FrontSearchController::class, 'searchOpenings'])->name('searchOpenings');
+Route::post('/search', [App\Http\Controllers\Front\FrontSearchController::class, 'searchOpenings'])->name('searchOpenings');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/master_universitas', function () {
+    return view('masters.universitas.index', ['active_menu' => 'master_universitas']);
+});
+Route::get('/', function () {
+    return view('masters.fakultas.index', ['active_menu' => 'master_fakultas']);
+});
+Route::get('/master_prodi', function () {
+    return view('masters.prodi.index', ['active_menu' => 'master_prodi']);
+});
+Route::get('/master_tahun_akademik', function () {
+    return view('masters.tahun_akademik.index', ['active_menu' => 'master_tahun_akademik']);
+});
+Route::get('/master_mitra', function () {
+    return view('masters.mitra.index', ['active_menu' => 'master_mitra']);
+});
+Route::get('/master_dosen', function () {
+    return view('masters.dosen.index', ['active_menu' => 'master_dosen']);
+});
+
+Route::get('/master_nilai_mutu', function () {
+    return view('masters.nilai_mutu.index', ['active_menu' => 'master_nilai_mutu']);
+});
+Route::get('/master_jenis_magang', function () {
+    return view('masters.jenis_magang.index', ['active_menu' => 'master_jenis_magang']);
+});
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
+    Route::get('/register', [RegisterAdminController::class, 'showRegistrationForm'])->name('register');
+    Route::post('/register', [RegisterAdminController::class, 'adminregister']);
+    Route::get('/login', [LoginAdminController::class, 'showLoginForm'])->name('auth.login_mitra');
+    Route::post('/login', [LoginAdminController::class, 'adminlogin'])->name('admin.login');
+    Route::get('/set-password/{token}', [SetPasswordController::class, 'showResetForm'])->name('set.password');
+    Route::post('/set-password', [SetPasswordController::class, 'reset'])->name('update.password');
+    Route::get('/dashboard', [LoginAdminController::class, 'dashboard'])->middleware(['auth', 'verified'])->name('dashboard');
+    Route::get('/logout', [LoginAdminController::class, 'logout'])->name('logout');
+});
