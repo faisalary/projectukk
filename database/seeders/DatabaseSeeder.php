3e4aae9c250fd536d9664ba533acaa58a6b312bf
fauziaul <auliafauzi835@gmial.com>
2023-11-22 10:57:44 +0700

    fix: databaseseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            // UniversitasSeeder::class,
-            // FakultasSeeder::class,
-            // ProdiSeeder::class,
+            UniversitasSeeder::class,
+            FakultasSeeder::class,
+            ProdiSeeder::class,
             RoleSeeder::class,
         ]);
 
