1938a092c0ca1672f55f5c27e39b7c3a269babf3
fauziaul <auliafauzi835@gmial.com>
2023-12-04 11:24:43 +0700

    Assign Role User

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -65,12 +65,15 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'username' => $data['username'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'isAdmin'=>2 
+            'isAdmin'=>2,
+            'id_industri'=> 21,
+            'id_mahasiswa' => 22
         ]);
+        $user->assignRole('user');
     }
 }
