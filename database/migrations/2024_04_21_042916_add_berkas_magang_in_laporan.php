6003c92ffb51b1582708c5a088295443768ac286
mitamutiara935 <mitamutiara476@gmail.com>
2024-04-21 13:58:50 +0700

    Add: column_in_laporan_akhir

---
+++ database/migrations/2024_04_21_042916_add_berkas_magang_in_laporan.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (Schema::hasColumn('laporan_akhir', 'durasi_magang') && Schema::hasColumn('laporan_akhir', 'berkas_magang')) {
+            Schema::table('laporan_akhir', function (Blueprint $table) {
+                $table->dropColumn('durasi_magang');
+                $table->dropColumn('berkas_magang');
+            });
+        } else {
+            Schema::table('laporan_akhir', function (Blueprint $table) {
+                $table->string('durasi_magang')->nullable();
+                $table->array('berkas_magang')->nullable();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('laporan_akhir', function (Blueprint $table) {
+            //
+        });
+    }
+};
