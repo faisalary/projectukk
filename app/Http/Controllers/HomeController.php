f806c6c4158af0bc8f5eedc11fc77d40e9ad6621
Imam Yabunaiya <withoutweed@student.telkomuniversity.ac.id>
2024-07-02 20:31:22 +0700

    WIP : Menampilkan mitra && lowongan di front page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Industri;
+use App\Models\JenisMagang;
 use Illuminate\Http\Request;
+use App\Models\LowonganMagang;
+use Illuminate\Support\Carbon;
 
 class HomeController extends Controller
 {
@@ -11,8 +15,49 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('landingpage.landingpage');
+
+        if ($request->component) {
+            return self::getComponent($request->component);
+        }
+
+        $jenisMagang = JenisMagang::all();
+        return view('landingpage.landingpage', compact('jenisMagang'));
+    }
+
+    private static function getComponent($type) 
+    {
+        if ($type == 'container-lowongan-magang') {
+            $lowongan = LowonganMagang::select(
+                'id_lowongan', 'intern_position', 'industri.namaindustri', 'industri.image', 
+                'created_at', 'lokasi', 'nominal_salary', 'durasimagang'
+            )
+            ->join('industri', 'industri.id_industri', '=', 'lowongan_magang.id_industri');
+
+            // pending dulu untuk yang terpopuler
+            // $lowonganTerpopuler = $lowongan->
+            // ------------------------------------
+
+            $lowonganTerbaru = $lowongan->limit(6)->orderBy('created_at', 'desc')->get()->transform(function ( $item, $key) {
+                $item->created_at = Carbon::parse($item->created_at)->diffForHumans(Carbon::now());
+                $item->durasimagang = implode(' dan ', json_decode($item->durasimagang));
+                $item->lokasi = implode(', ', json_decode($item->lokasi));
+                $item->image = url('storage/' . $item->image);
+                return $item;
+            });
+
+            return view('landingpage/components/lowongan', compact('lowonganTerbaru'))->render();
+        } else if ('container-mitra') {
+            $mitra = Industri::limit(6)->get()->transform(function ( $item, $key) {
+
+                $item->image = url('storage/' . $item->image);
+
+                return $item;
+            });
+
+            return view('landingpage/components/mitra', compact('mitra'))->render();
+        }
+
     }
 }
---
+++ resources/views/landingpage/components/mitra.blade.php
@@ -0,0 +1,22 @@
+<div class="row mx-5">
+    @foreach ($mitra as $item)
+        <div class="col-4 mt-5">
+            <div class="card">
+                <div class="card-body text-start">
+                    <div>
+                        <img class="img-thumbnail" src="{{ $item->image }}" style="max-height: 80px;" alt="admin.upload">
+                        <h4 style="text-align: left !important; -webkit-line-clamp: 1;text-overflow: ellipsis; overflow: hidden; display: -webkit-box; -webkit-box-orient: vertical; word-break: break-word; margin: top 100px;">
+                            {{ $item->namaindustri }}
+                        </h4>
+                        <div class="location" style="-webkit-line-clamp: 3;text-overflow: ellipsis; overflow: hidden; display: -webkit-box; -webkit-box-orient: vertical; word-break: break-word; margin: top 100px;">
+                            {{ $item->alamatindustri }}
+                        </div>
+                        <div class="button-container">
+                            <a href="/detail_perusahaan" class="btn btn-outline-primary mt-3">Lihat Perusahaan</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endforeach
+</div>
