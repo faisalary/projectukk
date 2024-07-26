f95ddf6516fc9684da0271cf0d67e2bc27437a80
Fugomika <farrasfadhila76@gmail.com>
2024-07-26 11:13:04 +0700

    fix : dikit

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -246,7 +246,7 @@
                         <div class="dropdown-divider"></div>
                     </li> --}}
                     <li>
-                        <a class="dropdown-item" href="pages-profile-user.html">
+                        <a class="dropdown-item" href="javascript:void(0);">
                             <i class="ti ti-user-check me-2 ti-sm"></i>
                             <span class="align-middle">My Profile</span>
                         </a>
