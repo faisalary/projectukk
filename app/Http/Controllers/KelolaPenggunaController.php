3e14f461bcd9370368503153c179ef75ec7447f8
Imam Yabunaiya <withoutweed@student.telkomuniversity.ac.id>
2024-08-01 09:36:06 +0700

    add : reset passwrd & status di kelola pengguna

--- app/Http/Controllers/KelolaPenggunaController.php
+++ app/Http/Controllers/KelolaPenggunaController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
 use App\Models\User;
+use App\Models\Dosen;
 use App\Helpers\Response;
 use App\Jobs\SendMailJob;
 use App\Mail\VerifyEmail;
-use App\Models\Role;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -36,13 +37,33 @@ class KelolaPenggunaController extends Controller
         ->editColumn('roles', function ($data) {
             return '<div class="text-center">' .$data->roles[0]->name. '</div>';
         })
-        ->addColumn('action', function ($data) {
+        ->addColumn('status', function ($data) {
             $result = '<div class="d-flex justify-content-center">';
-            $result .= '<a class="cursor-pointer text-warning" onclick="edit($(this))" data-id="' .$data->id. '"><i class="ti ti-edit"></i></a>';
-            $result .= '</>';
+            if ($data->hasRole('LKM') || $data->dosen->status) {
+                $result .= '<span class="badge rounded-pill bg-label-success">Active</span>';
+            } else {
+                $result .= '<span class="badge rounded-pill bg-label-danger">Inactive</span>';
+            }
+            $result .= '</div>';
+
             return $result;
         })
-        ->rawColumns(['roles', 'action'])
+        ->addColumn('action', function ($data) {
+            $icon = (isset($data->dosen) && $data->dosen->status) ? "ti-circle-x" : "ti-circle-check";
+            $color = (isset($data->dosen) && $data->dosen->status) ? "danger" : "primary";
+
+            $url = route('kelola_pengguna.update_status', $data->id);
+
+            $result = '<div class="d-flex justify-content-center">';
+            $result .= '<a class="cursor-pointer mx-1 text-warning" onclick="edit($(this))" data-id="' .$data->id. '"><i class="ti ti-edit"></i></a>';
+            $result .= '<a class="cursor-pointer mx-1 text-info" onclick="resetPassword($(this))" data-id="' .$data->id. '"><i class="ti ti-mail-forward"></i></a>';
+            if (!$data->hasRole('LKM')) {
+                $result .= '<a class="cursor-pointer update-status mx-1 text-'.$color.'" data-url="'.$url.'" data-function="afterUpdateStatus" data-id="' .$data->id. '"><i class="ti ' .$icon. '"></i></a>';
+            }
+            $result .= '</div>';
+            return $result;
+        })
+        ->rawColumns(['roles', 'status', 'action'])
         ->make(true);
     }
 
@@ -132,6 +153,49 @@ class KelolaPenggunaController extends Controller
             return Response::success(null, 'User successfully updated!');
         } catch (\Exception $e) {
             DB::rollBack();
+            return Response::errorCatch($e);
+        }
+    }
+
+    public function updateStatus($id) {
+        try {
+            $user = User::where('id', $id)->first();
+            if (!$user) return Response::error(null, 'User not found!');
+            if (!$user->hasAnyRole(['Dosen', 'Kaprodi'])) return Response::error(null, 'User not found!');
+
+            $dosen = Dosen::where('id_user', $user->id)->first();
+
+            $dosen->status = !$dosen->status;
+            $dosen->save();
+
+            return Response::success(null, 'Status successfully changed!');
+        } catch (\Exception $e) {
+            return Response::errorCatch($e);
+        }
+    }
+
+    public function resetPassword($id) {
+        try {
+            $user = User::where('id', $id)->first();
+            if (!$user) return Response::error(null, 'User not found!');
+
+            $passwordResetToken = DB::table('password_reset_tokens')->where('email', $user->email)->first();
+            if ($passwordResetToken) {
+                DB::table('password_reset_tokens')->where('email', $user->email)->delete();
+            }
+
+            $code = Str::random(60);
+            DB::table('password_reset_tokens')->insert([
+                'email' => $user->email,
+                'token' => $code,
+                'created_at' => now(),
+            ]);
+
+            $url = route('register.set-password', ['token' => $code]);
+            dispatch(new SendMailJob($user->email, new VerifyEmail($url)));
+
+            return Response::success(null, 'Password reset link has been sent!');
+        } catch (\Exception $e) {
             return Response::errorCatch($e);
         }
     }
