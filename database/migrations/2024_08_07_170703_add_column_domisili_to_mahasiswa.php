d7b2bdfdb6b3c280a4f682a041b9968c2bca6f9d
Imam Yabunaiya <withoutweed@student.telkomuniversity.ac.id>
2024-08-19 11:24:30 +0700

    fix : column in mahasiswa

--- database/migrations/2024_08_07_170703_add_column_domisili_to_mahasiswa.php
+++ database/migrations/2024_08_07_170703_add_column_domisili_to_mahasiswa.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('mahasiswa', function (Blueprint $table) {
-            $table->integer('kota_id')->nullable()->after('alamatmhs');
+            if (!Schema::hasColumn('mahasiswa', 'kota_id')) {
+                $table->integer('kota_id')->nullable()->after('alamatmhs');
+            }
             $table->foreign('kota_id')->references('id')->on('reg_regencies');
             $table->string('kodepos')->nullable()->after('kota_id');
         });
