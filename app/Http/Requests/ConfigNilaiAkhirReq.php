328c97eee662a787c1c20a91009f4618bb4d5000
Imam Yabunaiya <withoutweed@student.telkomuniversity.ac.id>
2024-09-03 19:20:43 +0700

    add : config nilai akhir magang

---
+++ app/Http/Requests/ConfigNilaiAkhirReq.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use PDO;
+
+class ConfigNilaiAkhirReq extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $validate = [
+            'id_prodi' => ['required', 'exists:program_studi,id_prodi'],
+            'nilai_pemb_lap' => ['required', 'min:1', 'max:100', function ( $attribute, $value, $fail) {
+                if (($value + $this->nilai_pemb_akademik) > 100) $fail('Nilai pembimbing lapangan + pembimbing akademik tidak boleh melebihi 100');
+            }],
+            'nilai_pemb_akademik' => ['required', 'min:1', 'max:100', function ( $attribute, $value, $fail) {
+                if (($value + $this->nilai_pemb_lap) > 100) $fail('Nilai pembimbing lapangan + pembimbing akademik tidak boleh melebihi 100');
+            }]
+        ];
+
+        return $validate;
+    }
+}
--- resources/views/masters/nilai_akhir/index.blade.php
+++ resources/views/masters/nilai_akhir/index.blade.php
@@ -23,59 +23,9 @@
                             <th>Program Studi</th>
                             <th>Presentase Nilai Pembimbing Lapangan(%)</th>
                             <th>Presentase Nilai Pembimbing Akademik (%)</th>
-                            <th>Aksi</th>
+                            <th class="text-center">Aksi</th>
                         </tr>
                     </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>
-                                D3 Sistem Informasi
-                            </td>
-                            <td>
-                                40
-                            </td>
-                            <td>
-                                60
-                            </td>
-                            <td class="d-flex">
-                                <button class="btn">
-                                    <i class="tf-icons ti ti-edit"></i>
-                                </button>
-                                <button class="btn">
-                                    <i class="tf-icons ti ti-trash"></i>
-                                </button>
-                                <button class="btn">
-                                    <i class="tf-icons ti ti-circle-check"></i>
-                                </button>
-                                {{-- <i data-feather='edit'></i> --}}
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>
-                                D3 Rekayasa Perangkat Lunak Aplikasi
-                            </td>
-                            <td>
-                                45
-                            </td>
-                            <td>
-                                55
-                            </td>
-                            <td class="d-flex">
-                                <button class="btn">
-                                    <i class="tf-icons ti ti-edit"></i>
-                                </button>
-                                <button class="btn">
-                                    <i class="tf-icons ti ti-trash"></i>
-                                </button>
-                                <button class="btn">
-                                    <i class="tf-icons ti ti-circle-x"></i>
-                                </button>
-                                {{-- <i data-feather='edit'></i> --}}
-                            </td>
-                        </tr>
-                    </tbody>
                 </table>
             </div>
         </div>
@@ -87,6 +37,75 @@
 
 @section('page_script')
 <script>
-      $('#table-master-nilai-akhir').DataTable({})
+    var table = $('#table-master-nilai-akhir').DataTable({
+        ajax: `{{ route('nilai_akhir.show') }}`,
+        serverSide: false,
+        processing: true,
+        deferRender: true,
+        type: 'GET',
+        destroy: true,
+        columns: [
+            { data: 'DT_RowIndex' },
+            { data: 'namaprodi', name: 'namaprodi' },
+            { data: 'nilai_pemb_lap', name: 'nilai_pemb_lap' },
+            { data: 'nilai_pemb_akademik', name: 'nilai_pemb_akademik' },
+            { data: 'aksi', name: 'aksi' }
+        ]
+    });
+
+    function edit(e) {
+        let modal = $('#modal-nilai-akhir');
+        let form = modal.find('form');
+
+        modal.find('.modal-title').html('Edit Presentase Nilai Akhir');
+        modal.find('form').attr('action', `{{ route('nilai_akhir.update', ['id' => ':id']) }}`.replace(':id', e.attr('data-id')));
+
+        $.ajax({
+            url: `{{ route('nilai_akhir.edit', ['id' => ':id']) }}`.replace(':id', e.attr('data-id')),
+            type: 'GET',
+            success: function (res) {
+                $.each(res.data, function (key, value) {
+                    form.find('[name="' + key + '"]').val(value).change(); 
+                });
+            }
+        });
+
+        modal.modal('show');
+    }
+
+    function destroy(e) {
+        let dataUrl = e.attr('data-url');
+
+        sweetAlertConfirm({
+            title: 'Apakah anda yakin ingin menghapus config?',
+            text: 'Data yang dihapus tidak akan bisa dikembalikan lagi',
+            icon: 'warning',
+            confirmButtonText: 'Ya, saya yakin',
+            cancelButtonText: 'Tidak'
+        }, function () {
+            $.ajax({
+                url: dataUrl,
+                type: 'DELETE',
+                headers: {'X-CSRF-TOKEN': "{{ csrf_token() }}"},
+                success: function (res) {
+                    afterChangeStatus(res)
+                }
+            });
+        });
+    }
+
+    function afterAction(response) {
+        $("#modal-nilai-akhir").modal('hide');
+        afterChangeStatus(response)
+    }
+
+    function afterChangeStatus(response) {
+        $('#table-master-nilai-akhir').DataTable().ajax.reload();
+    }
+
+    $('#modal-nilai-akhir').on('hidden.bs.modal', function () {
+        $(this).find('.modal-title').html('Tambah Presentase Nilai Akhir');;
+        $(this).find('form').attr('action', `{{ route('nilai_akhir.store') }}`);
+    });
 </script>
 @endsection
